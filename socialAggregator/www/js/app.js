022cb79267f011a3cb454cab46e3df2852526058
Paul Zadorozhniy <paulzadorozhniy@gmail.com>
2015-09-30 20:57:55 +0300

    settings screen with social buttons

--- socialAggregator/www/js/app.js
+++ socialAggregator/www/js/app.js
@@ -41,11 +41,11 @@ angular.module('starter', ['ionic', 'starter.controllers'])
     }
   })
 
-  .state('app.browse', {
-      url: '/browse',
+  .state('app.settings', {
+      url: '/settings',
       views: {
         'menuContent': {
-          templateUrl: 'templates/browse.html'
+          templateUrl: 'templates/settings.html'
         }
       }
     })
