f4e41d191b57bf2c6e7f497c7d4e663e336290dd
Paul Zadorozhniy <paulzadorozhniy@gmail.com>
2015-10-21 00:04:54 +0300

    (auth) added signup state

--- socialAggregator/www/js/app.js
+++ socialAggregator/www/js/app.js
@@ -25,7 +25,7 @@ angular.module('starter', ['ionic', 'starter.controllers'])
 .config(function($stateProvider, $urlRouterProvider) {
   $stateProvider
 
-    .state('app', {
+  .state('app', {
     url: '/app',
     abstract: true,
     templateUrl: 'templates/menu.html',
@@ -42,29 +42,36 @@ angular.module('starter', ['ionic', 'starter.controllers'])
   })
 
   .state('app.settings', {
-      url: '/settings',
+    url: '/settings',
       views: {
         'menuContent': {
           templateUrl: 'templates/settings.html'
         }
       }
     })
-    .state('app.playlists', {
-      url: '/playlists',
-      views: {
-        'menuContent': {
-          templateUrl: 'templates/playlists.html',
-          controller: 'PlaylistsCtrl'
-        }
+  .state('app.playlists', {
+    url: '/playlists',
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/playlists.html',
+        controller: 'PlaylistsCtrl'
       }
-    })
-
+    }
+  })
   .state('app.single', {
     url: '/playlists/:playlistId',
     views: {
       'menuContent': {
         templateUrl: 'templates/playlist.html',
         controller: 'PlaylistCtrl'
+      }
+    }
+  })
+  .state('app.signup', {
+    url: '/signup',
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/signup.html'
       }
     }
   });
